5a3f6770f627e78662e99e9b032633f8500e1aa7
kare-benjamin_h_rorvik <kbhrorvik@gmail.com>
2021-09-28 19:43:45 +0200

    Some bugs, but trying to make it work

--- UVM_PRACTICE_1/design/design.sv
+++ UVM_PRACTICE_1/design/design.sv
@@ -1,11 +1,19 @@
 // This is the SystemVerilog interface that we will use to connect
 // our design to our UVM testbench.
-interface dut_if;
-  logic clock, reset;
-  logic cmd;
-  logic [7:0] addr;
-  logic [7:0] data;
-  logic [7:0] x_or_data;
+interface dut_if(
+  input clock, 
+  input reset,
+  input cmd,
+  input [7:0] addr,
+  input [7:0] data
+);
+
+  clocking cb @(posedge clock);
+    output reset;
+    output cmd;
+    output addr;
+    output data;
+  endclocking
 endinterface
 
 `include "uvm_macros.svh"
@@ -14,13 +22,27 @@ endinterface
 // 
 // It is an empty design that simply prints a message whenever
 // the clock toggles.
-module dut(dut_if dif);
+module dut(
+  input logic clock0, reset0,
+  input logic cmd0,
+  input logic [7:0] addr0,
+  input logic [7:0] data0
+);
   import uvm_pkg::*;
-  always @(posedge dif.clock)
-    if (dif.reset != 1) begin
+  always @(posedge clock0)
+    if (reset0 != 1) begin
+      /*
       `uvm_info("DUT",
                 $sformatf("Received cmd=%b, addr=0x%2h, data=0x%2h",
-                          dif.cmd, dif.addr, dif.data), UVM_MEDIUM)
-    x_or_data <= data;            
+                          cmd0, addr0,data0), UVM_MEDIUM)
+      */
     end
 endmodule
+
+bind dut dut_if dut_if0(
+  .clock(clock0),
+  .reset(reset0),
+  .cmd(cmd0),
+  .addr(addr0),
+  .data(data0)
+);
